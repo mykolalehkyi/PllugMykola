e250e20df5c4b32b4d87755c0cdb80e29857ebe7
Mykola Lehkyi <lintey8@gmail.com>
2018-10-29 00:02:23 +0200

    Add H/W Lesson1 + rename image

---
+++ Homework/Lesson1.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public static void main(String[] args) {
+                int a=0,b=1,c=0;
+                for (int i=3;i<=9;i++){
+                    a+=i*i;
+                    b*=i-1;
+                }
+                c+=a+b;sds
+
+                System.out.println("Answer is: "+c);
+
+    }
+}
